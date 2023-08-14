ce067b70c5ffa4138f92f61706a7fae44a6ec85e
Guillaume <guillaume.verschave@gmail.com>
2023-08-14 22:01:21 +0200

    try responsive fixes

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import { Analytics } from "@vercel/analytics/react";
 export default function Home() {
   return (
-    <main className="flex justify-center items-center h-screen p-16 gap-16">
+    <main className="flex justify-center items-center h-screen p-16 gap-16 ">
       <Link href="build">
         <Image
           src="/img/hexagon-create-build.png"
