bc01c0aa25ac332157473dae32600026394e4a42
Guillaume <guillaume.verschave@gmail.com>
2023-08-14 22:32:36 +0200

    optimization

--- app/(routes)/build/page.tsx
+++ app/(routes)/build/page.tsx
@@ -926,6 +926,7 @@ const Build = () => {
                   alt="choisis une arme"
                   width={200}
                   height={200}
+                  priority
                   onClick={onOpenModalArmes}
                   className="hover:cursor-pointer hover:-translate-y-4 transition duration-200 ease-in-out animate-pulse"
                 />
